edc9315f530f32c106f032930da3cd2132613136
RIN <ckck803@naver.com>
2020-09-06 23:25:28 +0900

    add NotEmpty & Lob annotation
    Validation 추가와 content에 많은 양의 String을 저장하기 위해 Lob 추가

--- src/main/java/project/website/controller/dto/PostDto.java
+++ src/main/java/project/website/controller/dto/PostDto.java
@@ -3,11 +3,17 @@ package project.website.controller.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Lob;
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 public class PostDto {
 
+    @NotEmpty
     private String title;
     private String subtitle;
+
+    @Lob
     private String content;
 }
